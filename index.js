b443165a7da9a60e754020fd1c23670a7b76da0d
Errorbot1122 <629329@lkstevens.wednet.edu>
2020-12-11 22:20:42 +0000

    Edited index.js and added "mime-types" dependency

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-mud
+module.exports = (filePath) => {
+  let fileExists = false;
+
+  try {
+    if (fs.existsSync(filePath)) {
+      fileExists = true
+    }
+  } catch(err) {
+    throw new Error(err);
+  }
+
+  if (fileExists) {
+    if 
+  }
+}
