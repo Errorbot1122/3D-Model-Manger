0ed26e8afa61e36c56d0c6b8d4db8e38526b1483
Errorbot1122 <hustenlaf@gmail.com>
2020-12-12 11:01:23 -0800

    saving...

--- 3d-model-manger/index.js
+++ 3d-model-manger/index.js
@@ -3,6 +3,34 @@
 const mime = require('mime-types');
 const fs = require('fs');
 
+function IsString(x) {
+  return (typeof x === 'string' || x instanceof String)
+}
+function IsBool(x) {
+  return (typeof x === 'boolean' || x instanceof Boolean)
+}
+function IsObject(x) {
+  return (typeof x === 'object' || x instanceof Object)
+}
+
+class Vector {
+  constructor(...values) {
+    this.values = values
+  }
+}
+
+class Vector3 extends Vector {
+  constructor(x, y, z) {
+    super(x, y, z)
+  }
+}
+
+class Vertex {
+  constructor(x, y, z) {
+
+  }
+}
+
 function readModel(filePath) {
   let fileExists = false;
 
@@ -29,12 +57,44 @@ function readModel(filePath) {
   }
 };
 
-function parseRead(Modeldata) {
-  console.log(Modeldata.modelData)
+function parseRead(Modeldata, options) {
+  let keepComments = false
+
+  if (IsObject(options)) {
+    let keepComments = (IsBool(options.keepComments)) ? options.keepComments : false
+  }
+
+
+  let modelDataArray = Modeldata.modelData
+    .trim()
+    .split("\n");
+
+
+  if (!keepComments) {
+    removeCommentsRegex = /^#/
+
+    modelDataArrayFilterd = modelDataArray.filter((elem, i) => {
+
+      if (IsString(elem)) {
+
+        return !removeCommentsRegex.test(elem)
+      } else {
+
+        return true
+      }
+
+    });
+
+    modelDataArray = modelDataArrayFilterd
+  }
+
+
 }
 
 /* Testing */
+
 parseRead(readModel("../Test/cube.obj"));
+testVector = new Vector(1, 2, 4, 3);
 
 /* Exports */
 module.exports.readModel = readModel;
